7626adf1800d44c83ca0b187b7db5398e2d771db
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-01-27 11:16:37 +0200

    change them & add color to splash view body

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -11,6 +11,13 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(Assets.imagesLogo),
+        const SizedBox(
+          height: 4,
+        ),
+        const Text(
+          "Read Free Books",
+          textAlign: TextAlign.center,
+        ),
       ],
     );
   }
