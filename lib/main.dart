be9b46aaf3c68650b83756eb54be97cbc01ac6f5
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-01-27 09:52:09 +0200

    create splash feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -10,6 +11,8 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp();
+    return const GetMaterialApp(
+      home: SplashView(),
+    );
   }
 }
