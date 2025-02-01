85980c014bcc55b138568f14f50f66905d3f3afa
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-02-01 10:13:07 +0200

    create app styles

--- lib/features/home/presentation/view/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/view/widgets/best_seller_list_view_item.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
