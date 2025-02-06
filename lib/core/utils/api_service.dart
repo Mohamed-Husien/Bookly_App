2fc5dc32a45f8e4bad70297434845d95332c08c9
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-02-06 11:17:38 +0200

    create api service and create home repo implement classes

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,17 @@
+//raper class for all api services that could i use it in my app .
+
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
+  final Dio dio;
+  ApiService({
+    required this.dio,
+  });
+
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    var response = await dio.get("$_baseUrl$endPoint");
+
+    return response.data;
+  }
+}
