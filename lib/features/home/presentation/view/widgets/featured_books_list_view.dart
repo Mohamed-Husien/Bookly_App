a0fd4a18d8d09b4830e50cc6e41efadffc8c6629
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-02-08 08:15:53 +0200

    display featured books data part one

--- lib/features/home/presentation/view/widgets/featured_books_list_view.dart
+++ lib/features/home/presentation/view/widgets/featured_books_list_view.dart
@@ -1,22 +1,36 @@
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
   const FeaturedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.sizeOf(context).height * .3,
-      child: ListView.builder(
-          itemCount: 10,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (contxt, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: CustomBookImage(),
-            );
-          }),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.sizeOf(context).height * .3,
+            child: ListView.builder(
+                itemCount: 10,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (contxt, index) {
+                  return const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: CustomBookImage(),
+                  );
+                }),
+          );
+        } else if (state is FeaturedBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errorMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
