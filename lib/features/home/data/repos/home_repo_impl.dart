abfb2658ada7d88c055e5fc3c899233069cee6a2
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-02-09 13:23:57 +0200

    implement fetch similar books method

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -51,4 +51,25 @@ class HomeRepoImpl implements HomeRepo {
       return left(ServerError(errorMessage: e.toString()));
     }
   }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+      {required String category}) async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=relevance &q=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerError.fromDioException(e));
+      }
+      return left(ServerError(errorMessage: e.toString()));
+    }
+  }
 }
