a9641c2573959fade9e64f94381dd59d57fceebe
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-02-11 09:21:00 +0200

    search repo done

--- lib/features/search/presentation/manger/search_books_cubit/search_books_cubit.dart
+++ lib/features/search/presentation/manger/search_books_cubit/search_books_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/core/models/book_model/book_model.dart';
+import 'package:bookly_app/features/search/data/repos/search_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'search_books_state.dart';
 
 class SearchBooksCubit extends Cubit<SearchBooksState> {
-  SearchBooksCubit() : super(SearchBooksInitial());
+  SearchBooksCubit(this.searchRepo) : super(SearchBooksInitial());
+
+  final SearchRepo searchRepo;
+
+  Future<void> fetchSearchedBooks({required String category}) async {
+    emit(SearchBooksLoading());
+    final result = await searchRepo.fetchSearchedBooks(category: category);
+
+    result.fold(
+      (failure) => emit(SearchBooksFailure(errMessage: failure.errorMessage)),
+      (books) => emit(SearchBooksSuccess(books: books)),
+    );
+  }
 }
